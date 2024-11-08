c27a961b89ac8813f17f3dc17993d5cc04e268a7
VictorMtsx <joaovictormts.087@gmail.com>
2024-11-08 19:30:37 -0300

    configurando a imagem docker da aplicação

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'drizzle-kit'
 import { env } from './src/env'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './.migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 })
